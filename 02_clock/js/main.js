838cbb4fcc372c9d69f43f32f928eb582af41bcc
Sam Anderson <sam.anderson@joipolloi.com>
2016-12-29 18:52:13 +0000

    Cleanup of code and updated README

--- 02_clock/js/main.js
+++ 02_clock/js/main.js
@@ -1,13 +1,3 @@
-window.onload = function() {
-	const hands = {
-		'hour': document.querySelector(`.hour-hand`),
-		'minute': document.querySelector(`.minute-hand`),
-		'second': document.querySelector(`.second-hand`)
-	}
-	updateClock(hands);
-	var update = window.setInterval(updateClock,1000, hands);
-}
-
 function updateClock(hands){
 	const now = new Date;
 
@@ -22,11 +12,21 @@ function updateClock(hands){
 	updateHand(hands.second, time.seconds);	
 }
 
-function resetTransition(hand,limit){
-	hand.style.transition = limit ? 'inherit' : 'none';
+function resetTransition(hand,zero){
+	hand.style.transition = zero ? 'inherit' : 'none';
 }
 
 function updateHand(hand, time, interval=6) {
 	resetTransition(hand, time);
 	hand.style.transform = `rotate(${((90 + (time * interval)))}deg)`;
+}
+
+window.onload = function() {
+	const hands = {
+		'hour': document.querySelector(`.hour-hand`),
+		'minute': document.querySelector(`.minute-hand`),
+		'second': document.querySelector(`.second-hand`)
+	}
+	updateClock(hands);
+	var update = window.setInterval(updateClock,1000, hands);
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JS30
+
+## 01 : JS Drumkit
++ Key event handlers
++ Data attributes
++ HTML audio
+
+## 02 : JS & CSS Clock
++ Transforms
