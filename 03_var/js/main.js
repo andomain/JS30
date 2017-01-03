1598d4bfa02c7b8e7ecbcebb9f452914c0af0a45
Sam Anderson <sam.anderson@joipolloi.com>
2017-01-03 09:29:58 +0000

    Removing console logs

--- 03_var/js/main.js
+++ 03_var/js/main.js
@@ -1,7 +1,5 @@
 window.onload = function() {
-	console.log('Working');
 	const inputs = document.querySelectorAll('.controls input');
-	console.log(inputs);
 	inputs.forEach(input => input.addEventListener('change', handleUpdate));
 	// Future work: mod this so only triggers when clicked and moved
 	inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
